0e0626a397465a21757e5c0aaf6e6fadb80e32cf
daniff15 <dani.fig@ua.pt>
2021-12-14 22:26:12 +0000

    enter, exit, waiting store working

--- data-generation/main.py
+++ data-generation/main.py
@@ -3,80 +3,94 @@ from simulate import *
 from random import randint, choices
 import time
 
-store1_1 = Store("Zara" , 45, "10:00:00", "23:00:00")
-store2_1 = Store("Sport Zone" , 55, "10:00:00", "22:00:00")
-store3_1 = Store("Primark" , 70, "10:00:00", "23:00:00")
-store4_1 = Store("Modalfa" , 35, "10:00:00", "22:00:00")
-store1_2 = Store("Zara" , 50, "10:00:00", "22:00:00")
-store2_2 = Store("Aucham" , 100, "10:00:00", "22:00:00")
-store3_2 = Store("MEO" , 5, "10:00:00", "23:00:00")
-store4_2 = Store("Breshka" , 35, "10:00:00", "23:00:00")
-store1_3 = Store("Intimissimi" , 35, "10:00:00", "21:00:00")
-store2_3 = Store("Pull&Bear" , 45, "10:00:00", "22:00:00")
+store1_1 = Store("Zara", 10, "10:00:00", "23:00:00")
+store2_1 = Store("Sport Zone", 55, "10:00:00", "22:00:00")
+store3_1 = Store("Primark", 70, "10:00:00", "23:00:00")
+store4_1 = Store("Modalfa", 35, "10:00:00", "22:00:00")
+store1_2 = Store("Zara", 50, "10:00:00", "22:00:00")
+store2_2 = Store("Aucham", 100, "10:00:00", "22:00:00")
+store3_2 = Store("MEO", 5, "10:00:00", "23:00:00")
+store4_2 = Store("Breshka", 35, "10:00:00", "23:00:00")
+store1_3 = Store("Intimissimi", 35, "10:00:00", "21:00:00")
+store2_3 = Store("Pull&Bear", 45, "10:00:00", "22:00:00")
 
-mall1 = Mall("Mall 1", 5, [store1_1, store2_1, store3_1, store4_1], "9:00:00", "23:00:00")
-mall2 = Mall("Mall 2", 350, [store1_2, store2_2, store3_2, store4_2], "10:00:00", "23:00:00")
+mall1 = Mall("Mall 1", 50, [store1_1, store2_1,
+             store3_1, store4_1], "9:00:00", "23:00:00")
+mall2 = Mall("Mall 2", 350, [store1_2, store2_2,
+             store3_2, store4_2], "10:00:00", "23:00:00")
 mall3 = Mall("Mall 3", 250, [store1_3, store1_3], "9:30:00", "22:00:00")
 
 if __name__ == '__main__':
-    
-    pop = [1,2,3,4]
+
+    pop = [1, 2, 3, 4]
     weights = [0.30, 0.23, 0.23, 0.23]
-    
+    id = 0
+
     while True:
         #! Escolher um shopping e fazer o q tem de fazer
-        #primeira imp, apenas um mall
+        # primeira imp, apenas um mall
         #1-enter_mall; 2-exit_mall; 3-wait_mall; 4-no_wait_mall
-        
+
         if choices(pop, weights)[0] == 1:
-            #e condicao para verificar se respeitou o limite
-            if mall1.people_inside_mall < mall1.mall_limit:
-                if mall1.people_waiting_mall > 0:
-                    mall1.enterMall()
-                    mall1.people_waiting_mall -= 1
-                    """
-                    #and go shopping
-                    yes_no = randint(0,1)
-                    if yes_no == 1:
-                        #entra na loja
-                        print("entrou aqui")
-                        mall1.stores[0].enterStore()
-                        person1.change_status(Status.inside_store)
-                    """
+            # e condicao para verificar se respeitou o limite
+            if len(mall1.inside_mall_ids) < mall1.mall_limit:
+                if len(mall1.waiting_mall_ids) > 0:
+                    mall1.waiting_list_to_inside_mall()
+
+                    #TODO: METER AS CONDICOES DO Q A PESSOA VAI FAZER QUANDO ENTRAR NO SHOPPING
+                    if len(mall1.stores[0].inside_store_ids) >= mall1.stores[0].store_limit: #and RESPEITOU:
+                        mall1.stores[0].waiting_outside_Store(id)
+                    else:
+                        mall1.stores[0].enterStore(id) 
+
+                    id += 1
 
                 else:
-                    mall1.enterMall()
-                    """
-                    #and go shopping
-                    yes_no = randint(0,1)
-                    if yes_no == 1:
-                        #entra na loja
-                        print("entrou aqui")
-                        mall1.stores[0].enterStore()
-                        person1.change_status(Status.inside_store)
-                    """
+                    mall1.enterMall(id)
 
+                    #TODO: METER AS CONDICOES DO Q A PESSOA VAI FAZER QUANDO ENTRAR NO SHOPPING
+                    if len(mall1.stores[0].inside_store_ids) >= mall1.stores[0].store_limit: #and RESPEITOU:
+                        mall1.stores[0].waiting_outside_Store(id)
+                    else:
+                        mall1.stores[0].enterStore(id)
+
+                    id += 1
 
         elif choices(pop, weights)[0] == 2:
-            if mall1.people_inside_mall > 0:
-                mall1.exitMall()
+            if len(mall1.inside_mall_ids) > 0:
+                idx = randint(0 , len(mall1.inside_mall_ids) - 1)
+                print("idx - " , idx)
+                person_id = mall1.inside_mall_ids[idx]
+                mall1.exitMall(idx)
+
+                #remove person from store if its in one
+                if person_id in mall1.stores[0].inside_store_ids:
+                    idx_to_remove = mall1.stores[0].inside_store_ids.index(person_id)
+                    mall1.stores[0].exitStore(idx_to_remove)
+
+                if len(mall1.waiting_mall_ids) > 0:
+                    #!NOT WORKING YET
+                    mall1.waiting_list_to_inside_mall()
 
         elif choices(pop, weights)[0] == 3:
-            #e condicao para verificar se respeitou o limite
-            if mall1.people_inside_mall >= mall1.mall_limit:
-                mall1.waiting_outside_Mall()
+            # e condicao para verificar se respeitou o limite
+            if len(mall1.inside_mall_ids) >= mall1.mall_limit:
+                mall1.waiting_outside_Mall(id)
+                id += 1
 
         else:
             #e condicao para verificar se se cansou de esperar na fila
-            if mall1.people_inside_mall >= mall1.mall_limit and mall1.people_waiting_mall > 0:
-                mall1.stop_waiting_outside_Mall()
-
+            if len(mall1.inside_mall_ids) >= mall1.mall_limit and len(mall1.waiting_mall_ids) > 0:
+                idx_temp = randint(0 , len(mall1.waiting_mall_ids) - 1)
+                mall1.stop_waiting_outside_Mall(idx_temp)
 
         #!para depois contar quantas pessoas estao em cada cena, provavlemente vai se a cada lista ver os status de cada pessoa
         print("--------------START--------------")
-        print("Inside mall - " , mall1.people_inside_mall)
-        print("Waiting mall - " , mall1.people_waiting_mall)
-        print("INSIDE ZARA - ", mall1.stores[0].people_inside_store)
+        print("Inside mall - ", mall1.inside_mall_ids)
+        print("Waiting mall - ", mall1.waiting_mall_ids)
+        print("LEN - ", len(mall1.stores[0].inside_store_ids))
+        print("INSIDE ZARA - ", mall1.stores[0].inside_store_ids)
+        print("WAITING ZARA - ", mall1.stores[0].waiting_store_ids)
         print("--------------END--------------")
 
         """#escolha da loja para fazer cenas randoms
@@ -84,4 +98,4 @@ if __name__ == '__main__':
         print(mall1.stores[0].store_name)
         #print("Store - ", (mall1_1.stores)[1])
         exit(0)"""
-        time.sleep(0.1)
+        time.sleep(0.2)
--- data-generation/simulate.py
+++ data-generation/simulate.py
@@ -1,28 +1,36 @@
+from random import random
+
+
 class Mall:
     def __init__(self,mall_name, mall_limit,stores, opening_time, close_time):
-        self.people_inside_mall = 0
-        self.people_waiting_mall = 0
         self.mall_limit = mall_limit
         self.mall_name = mall_name
         self.stores = stores
         self.opening_time = opening_time
         self.close_time = close_time
+        self.inside_mall_ids = []
+        self.waiting_mall_ids = []
 
-    def enterMall(self):
+    def enterMall(self, id):
         #verificar se se pode entrar por causa do limite (verificar tambem se a pessoa respeitou ou nao)
-        self.people_inside_mall += 1
+        self.inside_mall_ids.append(id)
 
-    def exitMall(self):
+    def exitMall(self, id):
         #verficiar se ha gente no mall
-        self.people_inside_mall -= 1
+        self.inside_mall_ids.remove(self.inside_mall_ids[id])
     
-    def waiting_outside_Mall(self):
+    def waiting_outside_Mall(self, id):
         #verificar se o limit foi atingido
-        self.people_waiting_mall += 1
+        self.waiting_mall_ids.append(id)
 
-    def stop_waiting_outside_Mall(self):
-        self.people_waiting_mall -= 1
+    def stop_waiting_outside_Mall(self, id):
+        self.waiting_mall_ids.remove(self.waiting_mall_ids[id])
         #verificar se ha gente a espera
+
+    def waiting_list_to_inside_mall(self):
+        if len(self.waiting_mall_ids) > 0:
+            self.inside_mall_ids.append(self.waiting_mall_ids[0])
+            self.waiting_mall_ids.remove(self.waiting_mall_ids[0])
 
 class Store:
     def __init__(self, store_name, store_limit, opening_time, close_time):
@@ -30,14 +38,19 @@ class Store:
         self.store_limit = store_limit
         self.opening_time = opening_time
         self.close_time = close_time
-        self.people_inside_store = 0
-        self.people_waiting_store = 0
+        self.inside_store_ids = []
+        self.waiting_store_ids = []
 
-    def enterStore(self):
-        self.people_inside_store += 1
+    def enterStore(self, id):
+        self.inside_store_ids.append(id)
     
-    def exitStore(self):
-        self.people_inside_store -= 1
+    def exitStore(self, id):
+        self.inside_store_ids.remove(self.inside_store_ids[id])
 
-    def waiting_outside_Store(self):
-        self.people_waiting_store += 1 
+    def waiting_outside_Store(self, id):
+        self.waiting_store_ids.append(id) 
+
+    def waiting_list_to_inside_store(self):
+        if len(self.waiting_store_ids) > 0:
+            self.inside_store_ids.append(self.waiting_store_ids[0])
+            self.waiting_store_ids.remove(self.waiting_store_ids[0])
