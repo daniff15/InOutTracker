a31d177e12513254d1120b919587125942aff99a
Daniel Francisco <deny.jo@hotmail.com>
2021-12-18 16:03:38 +0000

    Add info to shopping cards

--- projweb/js/shoppings.js
+++ projweb/js/shoppings.js
@@ -1,13 +1,14 @@
-﻿function show(data) {
-    let cards = "";
-    for (let r of data) {
+﻿function showShoppings(shoppings) {
+    var cards = "";
+    for (var shopping of shoppings) {
         cards += 
         `<div class="col-lg-3 col-md-4 col-sm-6 col-12">
             <div class="card">
                 <img class="card-img-top" src="imgs/shopping2.jpeg" alt="Card image cap">
                 <div class="card-body">
-                    <h4 class="card-title">${r.name}</h4>
-                    <h5>${r.people_count}</h5>
+                    <h4 class="card-title">${shopping.name}</h4>
+                    <h5>${shopping.people_count} / ${shopping.max_capacity}</h5>
+                    <p>Open: ${shopping.opening_time}-${shopping.closing_time}</p>
                     <a href="shopping-center.html" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
@@ -20,7 +21,7 @@
     $.ajax({
         url: 'http://localhost:8000/api/v1/shoppings', 
         success: function(data) {
-            show(data);
+            showShoppings(data);
         },
         complete: function() {
             // Schedule the next request when the current one's complete
