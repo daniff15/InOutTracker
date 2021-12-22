430b5e7faad51d067461858e485bc877dc7f5492
SousaUA <hsousa@ua.pt>
2021-12-22 22:23:58 -0100

    fix

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ sleep 15
 cd projweb
 python -m SimpleHTTPServer 5500 &
 cd ../
-xdg-open http://127.0.0.1:5500/shoppings.html 
+xdg-open http://127.0.0.1:5500/shoppings.html &
 python3 projdatageneration/main.py >/dev/null
 
