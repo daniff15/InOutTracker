3603307b53236c2823d586190e434ad1d0c438c6
SousaUA <hsousa@ua.pt>
2021-12-22 23:00:08 -0100

    add pip install requirements to run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
+cd projdatageneration/
+pip3 install -r requirements.txt
+cd ../
 cd projAPI/InOutTracker
 ./mvnw install -Dmaven.test.skip
 cd ../../
 docker-compose up -d
 sleep 15
 cd projweb
-python -m SimpleHTTPServer 5500 &
+python -m SimpleHTTPServer 5500 & >/dev/null
 cd ../
 xdg-open http://127.0.0.1:5500/shoppings.html &
 python3 projdatageneration/main.py >/dev/null
