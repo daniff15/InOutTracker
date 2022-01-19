81cb552a9d9ba015fd6199691d1bd2d3ef92055d
SousaUA <hsousa@ua.pt>
2022-01-19 21:21:23 +0000

    Added populate DB and producer message

--- projdatageneration/main.py
+++ projdatageneration/main.py
@@ -1,169 +1,300 @@
 import random
 import requests
 from simulate import *
-from random import randint, choices
+from random import randint, choices, choice
 import time
 from copy import deepcopy
 import json
 import pulsar
 import sys
+from numpy import ma
+import datetime
+
 
 try:
-	client = pulsar.Client('pulsar://127.0.0.1:6650')
+	client = pulsar.Client('pulsar://pulsarclient:6650')
 	producer = client.create_producer(topic = 'persistent://public/default/ns1/people-count')
 except requests.exceptions.ConnectionError:
     print("Broker is not Running")
     exit(1)
-serviceURL = '127.0.0.1:8000/'  
-    
+serviceURL = 'springbootapi:8000/'
+
 try:
+    requests.post(f'http://{serviceURL}api/v1/users', json = {
+        "id": 0,
+        "type": 1,
+        "name": "Admin",
+        "username": "admin",
+        "email": "admin@inouttracker.com",
+        "password": "adminpass",
+    })
+    
     requests.post(f'http://{serviceURL}api/v1/shoppings', json ={
         "id": 0,
         "name": "Forum Aveiro",
         "opening_time": "09h00",
         "closing_time": "22h00",
-        "max_capacity": 200,
+        "max_capacity": 1500,
         "people_count": 0
     })
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 1,"shop_id": 0,"name": "Levi's","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 15, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 2,"shop_id": 0,"name": "FNAC","opening_time": "10h00", "closing_time": "22h00", "max_capacity": 50, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 3,"shop_id": 0,"name": "Sport Zone","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 155, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 4,"shop_id": 0,"name": "Zara","opening_time": "10h00", "closing_time": "22h00", "max_capacity": 140, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 5,"shop_id": 0,"name": "Pull & Bear", "opening_time": "09h00", "closing_time": "21h00", "max_capacity": 36, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 6,"shop_id": 0,"name": "Bershka","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 36, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 7,"shop_id": 0,"name": "Springfield","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 40, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 8,"shop_id": 0,"name": "Claire's","opening_time": "10h00", "closing_time": "20h00", "max_capacity": 14, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 9,"shop_id": 0,"name": "Body Shop","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 6, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 10,"shop_id": 0,"name": "Bimba Y Lola", "opening_time": "09h00", "closing_time": "22h00", "max_capacity": 15, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 11,"shop_id": 0,"name": "Boutique dos Relógios","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 8, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 12,"shop_id": 0,"name": "Calzedonia","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 12, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 13,"shop_id": 0,"name": "Decenio","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 20, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 14,"shop_id": 0,"name": "Quebramar","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 28, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 15,"shop_id": 0,"name": "Tiffosi","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 40, "people_count": 0})
 
-    requests.post(f'http://{serviceURL}api/v1/stores', json = {
+    requests.post(f'http://{serviceURL}api/v1/shoppings', json ={
         "id": 1,
-        "shop_id": 0,
-        "name": "Mi Store",
+        "name": "Glicinias Plaza",
         "opening_time": "09h00",
         "closing_time": "22h00",
-        "max_capacity": 15,
+        "max_capacity": 1200,
         "people_count": 0
     })
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 16,"shop_id": 1,"name": "Mi Store","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 15, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 17,"shop_id": 1,"name": "Worten","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 100, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 18,"shop_id": 1,"name": "Sport Zone","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 80, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 19,"shop_id": 1,"name": "Auchan","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 250, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 20,"shop_id": 1,"name": "C&A","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 80, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 21,"shop_id": 1,"name": "H&M","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 80, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 22,"shop_id": 1,"name": "Cortefiel","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 60, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 23,"shop_id": 1,"name": "Lefties","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 80, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 24,"shop_id": 1,"name": "New Yorker","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 50, "people_count": 0})
+    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 25,"shop_id": 1,"name": "Springfield","opening_time": "09h00", "closing_time": "21h00", "max_capacity": 40, "people_count": 0})
 
-    requests.post(f'http://{serviceURL}api/v1/stores', json = {
-        "id": 2,
-        "shop_id": 0,
-        "name": "FNAC",
-        "opening_time": "09h00",
-        "closing_time": "22h00",
-        "max_capacity": 50,
-        "people_count": 0
-    })
 
-    requests.post(f'http://{serviceURL}api/v1/stores', json = {"id": 3,"shop_id": 0,"name": "Sport Zone","opening_time": "09h00", "closing_time": "22h00", "max_capacity": 36, "people_count": 0})
+
+
 
     response_malls = requests.get(f'http://{serviceURL}api/v1/shoppings')
-    response_stores = requests.get(f'http://{serviceURL}api/v1/stores')
     response_malls = response_malls.json()
-    response_stores = response_stores.json()
-
 except requests.exceptions.ConnectionError:
     print("Server is not running")
     exit(1)
 
-stores = []
-for store in response_stores:
-    stores.append(Store(store['id'], store['name'], store['max_capacity'], store['opening_time'], store['closing_time']))
 
+malls = []
 for mall in response_malls:  
-    mall1 = Mall(mall['id'], mall["name"], mall['max_capacity'], stores, mall['opening_time'], mall['closing_time'])
-
+    stores = []
+    for store in mall['stores']:
+        stores.append(Store(store['id'], store['name'], store['max_capacity'], store['opening_time'], store['closing_time']))
+    malls.append(Mall(mall['id'], mall["name"], mall['max_capacity'], stores, mall['opening_time'], mall['closing_time']))
 
 def produce(message):
     producer.send(json.dumps(message).encode('utf-8'))
 
 if __name__ == '__main__':
-
+    #1-enter_mall; 2-exit_mall; 3-wait_mall; 4-no_wait_mall
     pop = [1, 2, 3, 4]
     weights = [0.30, 0.23, 0.23, 0.23]
-    id = 0
+
+    #1-respeitou 2-nao respeitou
+    pop_waitLine = [1, 2]
+    weights_waitLine = [0.90, 0.10]
+
+    #Starting day
+    hours_of_day = [00 , 00]
+    #format 00 - h - 00
+    #hours_of_day = ["00" , "00"]
+    #               horas, minutos
+    day = 0
+    #In case the store or mall are closed, do nothing
+    numbers_of_iters = 0
 
     while True:
+        #Choose the mall (random)
+
+        mall_idx = randint(0, len(malls) - 1)
+        mall = malls[mall_idx]
+
+        if hours_of_day == [00 , 00] and numbers_of_iters == 0:
+            print("NEW DAY")
+            day += 1
+            print("DAY - ", day)
+
         # primeira imp, apenas um mall e simulação em varias lojas
         #1-enter_mall; 2-exit_mall; 3-wait_mall; 4-no_wait_mall
+        idx_for_store = randint(0, len(mall.stores)-1)
+        store = mall.stores[idx_for_store]
 
-        idx_for_store = randint(0, len(mall1.stores)-1)
-        store = mall1.stores[idx_for_store]
-        print("LOJA ESCOLHIDA - ", store.store_name) 
+        if len(store.inside_store_ids) >= store.store_limit:
+            #print("ENTREI PARA " , mall.mall_name)
+            weights = [0.25, 0.40, 0.20, 0.15]
+        else:
+            weights = [0.30, 0.23, 0.23, 0.23]
 
-        if choices(pop, weights)[0] == 1:
+        val = choices(pop, weights)[0]
+
+        #mall or store are closed
+        #if mall is closed than all stores are closed, so check first mall
+        opening_mall = mall.opening_time.split("h")
+        for i in range(len(opening_mall)):
+            opening_mall[i] = int(opening_mall[i])
+
+        closing_mall = mall.close_time.split("h")
+        for i in range(len(closing_mall)):
+            closing_mall[i] = int(closing_mall[i])
+
+        if (opening_mall[0] > hours_of_day[0]): #shopping fechado
+            val = 5
+        elif (opening_mall[0] == hours_of_day[0] and opening_mall[1] > hours_of_day[1]): #shopping fechado
+            val = 5
+        elif (closing_mall[0] < hours_of_day[0]):
+            val = 5
+        elif (closing_mall[0] == hours_of_day[0] and closing_mall[1] <= hours_of_day[1]):
+            val = 5
+
+
+        if val == 5:
+            mall.inside_mall_ids = []
+            mall.waiting_mall_ids = []
+            for store in mall.stores:
+                if len(store.inside_store_ids) > 0:
+                    store.inside_store_ids = []
+                if len(store.waiting_store_ids) > 0:
+                    store.waiting_store_ids = []
+            for shopping in malls:
+                if mall.close_time == shopping.close_time:
+                    shopping.inside_mall_ids = []
+                    shopping.waiting_mall_ids = []
+                    for store in shopping.stores:
+                        if len(store.inside_store_ids) > 0:
+                            store.inside_store_ids = []
+                        if len(store.waiting_store_ids) > 0:
+                            store.waiting_store_ids = []
+        #print("1-enter_mall; 2-exit_mall; 3-wait_mall; 4-no_wait_mall")
+        #print("VAL - ", val)
+        #print("WEIGHTS - ", weights)
+        #print("LOJA ESCOLHIDA - ", store.store_name)
+
+        if val == 1:
             # e condicao para verificar se respeitou o limite
-            if len(mall1.inside_mall_ids) < mall1.mall_limit:
-                if len(mall1.waiting_mall_ids) > 0:
-                    mall1.waiting_list_to_inside_mall()
-
-                    #TODO: METER AS CONDICOES DO Q A PESSOA VAI FAZER QUANDO ENTRAR NO SHOPPING
-                    if len(store.inside_store_ids) >= store.store_limit: #and RESPEITOU:
-                        store.waiting_outside_Store(id)
-                    else:
-                        store.enterStore(id) 
-                    id += 1
-
+            if len(mall.inside_mall_ids) < mall.mall_limit:
+                mall.enterMall(mall.people_id)
+                #Condicao que verifica se a pessoa respeitou a fila de espera
+                respeitou_int = choices(pop_waitLine, weights_waitLine)[0]
+                if respeitou_int == 1:
+                    respeitou = True
                 else:
-                    mall1.enterMall(id)
+                    respeitou = False
+                if len(store.inside_store_ids) >= store.store_limit and respeitou:
+                    store.waiting_outside_Store(mall.people_id)
+                else:
+                    #!Supostamente ja entra quando nao respeita
+                    store.enterStore(mall.people_id)
+                mall.people_id += 1
 
-                    #TODO: METER AS CONDICOES DO Q A PESSOA VAI FAZER QUANDO ENTRAR NO SHOPPING
-                    if len(store.inside_store_ids) >= store.store_limit: #and RESPEITOU:
-                        store.waiting_outside_Store(id)
-                    else:
-                        store.enterStore(id)
-
-                    id += 1
-
-        elif choices(pop, weights)[0] == 2:
-            if len(mall1.inside_mall_ids) > 0:
-                idx = randint(0 , len(mall1.inside_mall_ids) - 1)
-                print("idx - " , idx)
-                person_id = mall1.inside_mall_ids[idx]
-                mall1.exitMall(idx)
-
+        elif val == 2:
+            if len(mall.inside_mall_ids) > 0:
+                idx = randint(0 , len(mall.inside_mall_ids) - 1)
+                person_id = mall.inside_mall_ids[idx]
+                mall.exitMall(idx)
                 #remove person from store if its in one
-                for loja in mall1.stores:
+                for loja in mall.stores:
                     if person_id in loja.inside_store_ids:
                         idx_to_remove = loja.inside_store_ids.index(person_id)
                         loja.exitStore(idx_to_remove)
                         if len(loja.waiting_store_ids) > 0:
                             loja.waiting_list_to_inside_store()
-
                 #remove person from waiting list of store if the person leaves the mall 
-                for loja in mall1.stores:
+                for loja in mall.stores:
                     if person_id in loja.waiting_store_ids:
                         idx_to_remove = loja.waiting_store_ids.index(person_id)
                         loja.stop_waiting_store(idx_to_remove)
+                if len(mall.waiting_mall_ids) > 0:
+                    val = mall.waiting_list_to_inside_mall()
 
-                if len(mall1.waiting_mall_ids) > 0:
-                    mall1.waiting_list_to_inside_mall()
+                    if len(store.inside_store_ids) >= store.store_limit: #and respeitou:
+                        store.waiting_outside_Store(val)
+                    else:
+                        store.enterStore(val)
+                        mall.people_id += 1 
 
-        elif choices(pop, weights)[0] == 3:
+        elif val == 3:
             # e condicao para verificar se respeitou o limite
-            if len(mall1.inside_mall_ids) >= mall1.mall_limit:
-                mall1.waiting_outside_Mall(id)
-                id += 1
+            respeitou_int = choices(pop_waitLine, weights_waitLine)[0]
 
-        else:
+            if respeitou_int == 1:
+                respeitou = True
+            else:
+                respeitou = False
+
+            if len(mall.inside_mall_ids) >= mall.mall_limit and respeitou:
+                mall.waiting_outside_Mall(mall.people_id)
+                mall.people_id += 1
+            elif len(mall.inside_mall_ids) >= mall.mall_limit and not respeitou:
+                mall.enterMall(mall.people_id)
+
+                #nao respeitou e agora tem de entrar numa loja
+                respeitou_int = choices(pop_waitLine, weights_waitLine)[0]
+
+                if respeitou_int == 1:
+                    respeitou = True
+                else:
+                    respeitou = False
+
+                if len(store.inside_store_ids) >= store.store_limit and respeitou:
+                    store.waiting_outside_Store(mall.people_id)
+                else:
+                    #!Supostamente ja entra quando nao respeita
+                    store.enterStore(mall.people_id)
+                mall.people_id += 1
+
+        elif val == 4:
             #e condicao para verificar se se cansou de esperar na fila
-            if len(mall1.inside_mall_ids) >= mall1.mall_limit and len(mall1.waiting_mall_ids) > 0:
-                idx_temp = randint(0 , len(mall1.waiting_mall_ids) - 1)
-                mall1.stop_waiting_outside_Mall(idx_temp)
+            if len(mall.inside_mall_ids) >= mall.mall_limit and len(mall.waiting_mall_ids) > 0:
+                idx_temp = randint(0 , len(mall.waiting_mall_ids) - 1)
+                mall.stop_waiting_outside_Mall(idx_temp)
+            
+        else: #val == 5 #store or mall are closed
+            #just pass the time
+            pass
+            
+        #To see when to increase the hours_of_day    
+        numbers_of_iters += 1
 
-        ''' implementation for one mall '''
-        stores_capacity = {mall1.id: len(mall1.inside_mall_ids)}
-        for store in mall1.stores:
+        #TODO: change this to work with multiple malls
+        stores_capacity = {mall.id: len(mall.inside_mall_ids)}
+        for store in mall.stores:
             stores_capacity[store.id] = len(store.inside_store_ids)
+
         produce(stores_capacity)
 
-        #!para depois contar quantas pessoas estao em cada cena, len(lst)
-        print("--------------START--------------")
-        print("Inside mall - ", mall1.inside_mall_ids)
-        print("Waiting mall - ", mall1.waiting_mall_ids)
-        print("LEN - ", len(store.inside_store_ids))
-        for store in mall1.stores:
-            print("INSIDE  ", store.store_name , " - " , store.inside_store_ids)           
-            print("WAITING ", store.store_name, " - ", store.waiting_store_ids)
+
+        for shopping in malls:
+            print("--------------START--------------")
+            print("SHOPPING ESCOLHIDO - ", shopping.mall_name)
+            print("Inside mall - ", shopping.inside_mall_ids)
+            print("Waiting mall - ", shopping.waiting_mall_ids)
+            print("LEN - ", len(shopping.inside_mall_ids))
+            for store in shopping.stores:
+                print("INSIDE  ", store.store_name , " - " , store.inside_store_ids)           
+                print("WAITING ", store.store_name, " - ", store.waiting_store_ids)
+                print("LEN - ", len(store.inside_store_ids))
+            print("--------------END--------------")
+
+        #JUST TO TEST INCREASE ON HOURS_OF_DAY
+        if numbers_of_iters == 5:
+            numbers_of_iters = 0
+            if hours_of_day == [23 , 45]:
+                hours_of_day = [00 , 00]
+            elif hours_of_day[1] != 45:
+                hours_of_day[1] += 15
+            elif hours_of_day[1] == 45:
+                hours_of_day[0] += 1
+                hours_of_day[1] = 00
+
+        print("HOURS OF DAY  - " , hours_of_day)
+        print("-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|")
         
-        print("--------------END--------------")
-
-        """#escolha da loja para fazer cenas randoms
-        #list_idx = randint(0, len(mall1.stores))
-        print(store.store_name)
-        #print("Store - ", (mall1_1.stores)[1])
-        exit(0)"""
-
-        time.sleep(0.1)
-#client.close()
+        time.sleep(0.025)
