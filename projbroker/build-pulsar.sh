3338dfb929dcc4a76aea958c277ac7cf91847ee0
SousaUA <hsousa@ua.pt>
2022-01-19 10:32:49 +0000

    delete broker and db directories

--- projbroker/build-pulsar.sh
+++
@@ -1,8 +0,0 @@
-docker run -it \
-  --name pulsar-standalone \
-  -p 6650:6650 \
-  -p 8080:8080 \
-  --mount source=pulsardata,target=/pulsar/data \
-  --mount source=pulsarconf,target=/pulsar/conf \
-  apachepulsar/pulsar:2.8.1 \
-  bin/pulsar standalone
