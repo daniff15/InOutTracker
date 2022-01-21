646e9c694d2bf8b76195d3f29ef08aec1a9b4436
Daniel Francisco <deny.jo@hotmail.com>
2022-01-21 22:54:30 +0000

    Add feedback on whether a store is a favorite or not

--- projweb/content/js/shoppingCenter.js
+++ projweb/content/js/shoppingCenter.js
@@ -2,6 +2,34 @@
     var queryString = location.search.substring(1);
     var a = queryString.split("|");
     var shoppingId = a[0];
+
+    var isLoggedIn = false;
+    user = readCookie('login');
+    if(user) {
+        user = JSON.parse(user);
+        isLoggedIn = true;
+        $("#account").html(
+            `<li class="nav-item">
+                <a class="nav-link" href="account.html">${user["username"]}</a>
+            </li>`
+        );
+    }
+
+    var favorites = $.parseJSON($.ajax({
+        url: "http://" + self.location.hostname + ":8000/api/v1/user/" + user["id"] + "/favorites",
+        async: false
+    }).responseText);
+
+    // function showFavorites() {
+    //     for(var relation of favorites) {
+    //         storeId = "#" + relation.store_id;
+    //         console.log(storeId);
+    //         if($(storeId)) {
+    //             $(storeId).find("button").html(`<i class="fa fa-heart" aria-hidden="true"></i>`)
+    //         }
+    //     }
+    // }
+
     function showStores(stores) {
         if(isLoggedIn) {
             var head = 
@@ -17,15 +45,21 @@
                 body +=
                 `<tr id="${store.id}">
                     <td>
-                        <button class="btn btn-outline-danger fav-shop-button">
-                            <i class="fa fa-heart-o" aria-hidden="true"></i>
-                        </button>
+                        <button class="btn btn-outline-danger fav-shop-button">`
+                if(!containsObject(store.id, favorites)) {
+                    body += `<i class="fa fa-heart-o" aria-hidden="true"></i>`
+                }else {
+                    body += `<i class="fa fa-heart" aria-hidden="true"></i>`
+                }
+                body +=
+                        `</button>
                     </td>
                     <th scope="row" style="vertical-align: middle;">${store.name}</th>
                     <td class="text-right" style="vertical-align: middle;">${store.people_count}/${store.max_capacity}</td>
                 </tr>`
             }
             body += "</tbody>"
+            // showFavorites();
         }else {
             var head = 
                 `<thead>
@@ -75,16 +109,15 @@
         return '';
     }
 
-    var isLoggedIn = false;
-    user = readCookie('login');
-    if(user) {
-        user = JSON.parse(user);
-        isLoggedIn = true;
-        $("#account").html(
-            `<li class="nav-item">
-                <a class="nav-link" href="account.html">${user["username"]}</a>
-            </li>`
-        );
+    function containsObject(obj, list) {
+        var i;
+        for (i = 0; i < list.length; i++) {
+            if (list[i] == obj) {
+                return true;
+            }
+        }
+    
+        return false;
     }
 
     $(document).on("click", ".fav-shop-button", function() {
@@ -93,13 +126,25 @@
             user_id: user["id"],
             store_id: shopId,
         };
-        $.ajax({
-            url: "http://" + self.location.hostname + ":8000/api/v1/add/favorite",
-            type: "POST",
-            data: JSON.stringify(data),
-            contentType: "application/json",
-        }).done(function (data) {
-            console.log(data);
-        });
+        if(!containsObject(shopId, favorites)) {
+            favorites.push(shopId);
+            $.ajax({
+                url: "http://" + self.location.hostname + ":8000/api/v1/add/favorite",
+                type: "POST",
+                data: JSON.stringify(data),
+                contentType: "application/json",
+            }).done(function (data) {
+                console.log(data);
+            });
+        }else {
+            $.ajax({
+                url: "http://" + self.location.hostname + ":8000/api/v1/remove/favorite",
+                type: "DELETE",
+                data: JSON.stringify(data),
+                contentType: "application/json",
+            }).done(function (data) {
+                console.log(data);
+            });
+        }
     });
 });
